991a272783d726bf8efaac2491d1d26481ba87d7
Lucas Costa <lucasrodcosta@gmail.com>
2019-01-25 10:15:57 -0200

    Move Lua code to a file

---
+++ src/throttling.lua
@@ -0,0 +1,38 @@
+local throttling = {}
+
+throttling.rate_for = function(redis_client, key)
+  local current_time = math.floor(ngx.now())
+  local current_minute = math.floor(current_time / 60) % 60
+  local past_minute = current_minute - 1
+  local current_key = key .. current_minute
+  local past_key = key .. past_minute
+
+  local resp, err = redis_client:get(past_key)
+  if err then
+    return nil, err
+  end
+
+  if resp == ngx.null then
+    resp = "0"
+  end
+
+  local last_counter = tonumber(resp)
+
+  resp, err = redis_client:incr(current_key)
+  if err then
+    return nil, err
+  end
+
+  local current_counter = tonumber(resp) - 1
+
+  resp, err = redis_client:expire(current_key, 2 * 60)
+  if err then
+    return nil, err
+  end
+
+  -- strongly inspired by https://blog.cloudflare.com/counting-things-a-lot-of-different-things/
+  local current_rate = last_counter * ((60 - (current_time % 60)) / 60) + current_counter
+  return current_rate, nil
+end
+
+return throttling
